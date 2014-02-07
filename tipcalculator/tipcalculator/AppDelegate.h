32bb8e63e3b5926227db5033e3d1e1f480184693
Aaron Carambula <carambula@fb.com>
2014-02-07 01:34:16 -0800

    Tip calculator
    First homework assignment. http://vimeo.com/74764846

---
+++ tipcalculator/tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Aaron Carambula on 2/7/14.
+//  Copyright (c) 2014 Aaron Carambula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/tipcalculator/TipViewController.m
@@ -0,0 +1,62 @@
+//
+//  TipViewController.m
+//  tipcalculator
+//
+//  Created by Aaron Carambula on 2/7/14.
+//  Copyright (c) 2014 Aaron Carambula. All rights reserved.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *billTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
+- (IBAction)onTap:(id)sender;
+
+- (void)updateValues;
+
+@end
+
+@implementation TipViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+      self.title = @"Tabbit";
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self updateValues];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)onTap:(id)sender {
+  [self.view endEditing:YES];
+  [self updateValues];
+}
+- (void)updateValues {
+  float billAmount = [self.billTextField.text floatValue];
+  NSArray *tipValues = @[@(0.18), @(0.2), @(0.22)];
+  float tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex] floatValue];
+  float totalAmount = tipAmount + billAmount;
+  self.tipLabel.text = [NSString stringWithFormat:@"%0.2f", tipAmount];
+  self.totalLabel.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
+}
+
+@end
